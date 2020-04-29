965ba408164f6487e56f8f11fafc6e808b5dd8b6
mohmagdy1016 <61683389+mohmagdy1016@users.noreply.github.com>
2020-04-30 01:09:19 +0200

    script1

---
+++ upload_docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+  export username="mohmagdy1016"
+	 export path=udacitycapstoneproject
+	 export version=0.1
+	 export dockerpath=$username/$path;
+	imageId=$(docker images -q $dockerpath:latest)
+	docker tag $imageId $dockerpath:$version
+	docker push $dockerpath
