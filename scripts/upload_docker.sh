31d7f0a986ed7e104366b05cc696e3fe26058bd0
mohmagdy1016 <61683389+mohmagdy1016@users.noreply.github.com>
2020-04-29 23:55:07 +0200

    allfiles

---
+++ scripts/upload_docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+  export username="mohmagdy1016"
+	 export path=fintest
+	 export version=0.1
+	 export dockerpath=$username/$path;
+	imageId=$(docker images -q $dockerpath:latest)
+	docker tag $imageId $dockerpath:$version
+	docker push $dockerpath
