326d42c01e05abb0e20693e0e8848550cc264bd7
mohmagdy1016 <61683389+mohmagdy1016@users.noreply.github.com>
2020-04-30 02:17:28 +0200

    script7

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
-
-
-  export username="mohmagdy1016"
+     export username="mohmagdy1016"
 	 export path=udacitycapstoneproject
 	 export version=0.1
 	 export dockerpath=$username/$path;
