b72dfc34de17d663b388f29d4710d8da84def9e8
mohmagdy1016 <61683389+mohmagdy1016@users.noreply.github.com>
2020-05-01 00:23:20 +0200

    Delete upload_docker.sh

--- upload_docker.sh
+++
@@ -1,9 +0,0 @@
-     sh """\
-     #!/bin/sh
-	 export username="mohmagdy1016"
-	 export path=udacitycapstoneproject
-	 export version=0.1
-	 export dockerpath=$username/$path;
-	imageId=$(docker images -q $dockerpath:latest)
-	docker tag $imageId $dockerpath:$version
-	docker push $dockerpath
